2018b7a4fc3b61965f73c4b8122dd0ac75e16b07
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2024-12-08 17:10:31 +0000

    Apply Android lint to JVM modules

--- build-logic/conventions/src/main/java/net/c306/conventions/plugins/KotlinLibraryConventionPlugin.kt
+++ build-logic/conventions/src/main/java/net/c306/conventions/plugins/KotlinLibraryConventionPlugin.kt
@@ -2,6 +2,7 @@ package net.c306.conventions.plugins
 
 import net.c306.conventions.shared.addBaseDependencies
 import net.c306.conventions.shared.hasTestFixtures
+import net.c306.conventions.shared.libs
 import net.c306.conventions.shared.setupDetekt
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -19,7 +20,8 @@ class KotlinLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
-                apply("org.jetbrains.kotlin.jvm")
+                apply(libs.findPlugin("kgp.jvm").get().get().pluginId)
+                apply(libs.findPlugin("android.lint").get().get().pluginId)
 
                 setupDetekt()
 
