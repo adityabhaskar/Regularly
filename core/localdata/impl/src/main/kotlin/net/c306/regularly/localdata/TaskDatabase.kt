16c851e1080646b967ce3b50df680d336f1c5f59
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2024-12-08 16:11:18 +0000

    Add type converters

--- core/localdata/impl/src/main/kotlin/net/c306/regularly/localdata/TaskDatabase.kt
+++ core/localdata/impl/src/main/kotlin/net/c306/regularly/localdata/TaskDatabase.kt
@@ -2,6 +2,7 @@ package net.c306.regularly.localdata
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import net.c306.regularly.localdata.models.EntryEntity
 import net.c306.regularly.localdata.models.TagEntity
 import net.c306.regularly.localdata.models.TaskAndTagEntity
@@ -16,6 +17,7 @@ import net.c306.regularly.localdata.models.TaskEntity
     ],
     version = 1,
 )
+@TypeConverters(Converters::class)
 internal abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
 }
