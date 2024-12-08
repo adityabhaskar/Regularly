655e6e65f4f519d86c7cf7b85a2c18c14d1063a4
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2024-12-08 12:24:59 +0000

    Create empty api-impl modules for local data

---
+++ core/localdata/api/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+     alias(libs.plugins.library.kotlin)
+
+    // Optional: parcelize
+    // id("kotlin-parcelize")
+    // Optional: use compose in module
+    // alias(libs.plugins.compose.library)
+    // Optional: Use hilt in module
+    // alias(libs.plugins.hilt)
+}
+
+dependencies {
+}
